55b439405aae0916400c8dc07ef6d58166116b27
Ricardo <jony.ricardo95@gmail.com>
2019-04-30 16:19:46 +0100

    Reverse Bits added
    Function that reverse all bits in a uint16, Testing I2C for temperature sensor

--- example.cpp
+++ example.cpp
@@ -13,10 +13,26 @@ void sigalrm_handler(int sig) {
 }
 
 void check(int err) {
-	if (err!=0) {
+	if (err<0) {
 		exit(2);
 	}
 }
+
+uint16_t reverse_int16(uint16_t v) {
+
+	uint16_t r = v; // r will be reversed bits of v; first get LSB of v
+	int s = sizeof(v) * 8 - 1; // extra shift needed at end
+
+	for (v >>= 1; v; v >>= 1)
+	{
+	  r <<= 1;
+	  r |= v & 1;
+	  s--;
+	}
+	r <<= s; // shift when v's highest bits are zero
+	return r;
+}
+
 int main(void) {
 	cout << "Hello IOT2020." << endl;
 
@@ -32,38 +48,54 @@ int main(void) {
 	ext_button->dir(mraa::DIR_IN);
 
 	Gpio* user_led = NULL;
-	user_led = new mraa::Gpio(13, true, false);
+	user_led = new mraa::Gpio(1, true, false);
 	user_led->dir(mraa::DIR_OUT);
 
+	Spi* led_mat = NULL;
+	led_mat = new mraa::Spi(0);
+	led_mat -> frequency(50000);
+	uint16_t spi_word;
+
+	Gpio* load = NULL;
+	load = new mraa::Gpio(9, true, false);
+	load->dir(mraa::DIR_OUT);
+
 	Gpio* led = NULL;
-	led = new mraa::Gpio(10);
+	led = new mraa::Gpio(0);
 	led->dir(mraa::DIR_OUT);
 
 	Pwm* pwm = NULL;
-	pwm = new mraa::Pwm(9);
+	pwm = new mraa::Pwm(3);
 
 	Aio* a_pin = NULL;
 	a_pin = new mraa::Aio(0);
 
 	float w;
 
-/*
 	mraa::I2c* temp;
 	temp = new mraa::I2c(0);
 	temp -> frequency(I2C_STD);
-	temp -> address(0x4d);
+	temp -> address(0x18);
 
-	uint8_t temperature;
-*/
+	uint16_t temperature, temp1, temp2;
+	float tempe;
+
 	for (;;) {
 		if(!(user_button ->read())){
 			alarm(0);
 			check(user_led -> write(0));
 			exit(1);
 		}
+		/*
+		spi_word=0x10F0;
+		load -> write(0);
+		led_mat -> transfer_word(&spi_word, NULL, 2);
+		load -> write(1);
+		*/
 		w=a_pin->readFloat();
 		pwm -> write(w/0.67);
 		check(led -> write(ext_button -> read()));
+
 
 		if (flag) {
 
@@ -72,15 +104,43 @@ int main(void) {
 			} else {
 				check(user_led->write(1));
 			}
+
 			w=a_pin->readFloat();
 			led -> write(w/0.67);
 			w=w*5;
 			printf("%4.2f V\n", w);
 
-/*
-			temperature = temp -> readReg(0x00);
-			cout << temperature <<endl;
-*/
+
+			temperature = temp -> readWordReg(0b00000101);
+
+			temperature = reverse_int16(temperature);
+			temperature = (temperature & 0x0FFF)/16;
+
+			/*temp1 = (temperature & 0x00FF);
+			temp2 = (temperature & 0xFF00) >> 8;
+
+			//Convert the temperature data
+			//First Check flag bits
+			if ((temp2 & 0x80) == 0x80){ 		//TA ³ TCRIT
+			}
+
+			if ((temp2 & 0x40) == 0x40){ 		//TA > TUPPER
+			}
+
+			if ((temp2 & 0x20) == 0x20){ 		//TA < TLOWER
+			}
+
+			temp2 = temp2 & 0x1F;	 			//Clear flag bits
+
+			if ((temp2 & 0x10) == 0x10){ 		//TA < 0°C
+			temp2 = temp2 & 0x0F; 				//Clear SIGN
+			tempe = 256 - (temp2 * 16 + temp1 / 16);
+			}else 								//TA ³ 0°C
+			tempe = (temp2 * 16 + temp1 / 16);
+			*/
+
+			printf("%4.2f °C\n", temperature);
+
 			alarm(1);
 			flag=0;
 		}
