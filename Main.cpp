8ad21e8420a1521f5053c200d7aef65a9d2e517b
João SImões <jony.ricardo95@gmail.com>
2019-07-11 00:17:22 +0100

    New set of Files

--- Main.cpp
+++ Main.cpp
@@ -1,57 +1,44 @@
+#include "WorkerThread.h"
+#include "Fault.h"
 #include <iostream>
-#include <stdio.h>
-#include <signal.h>
+
 using namespace std;
-#include "mraa.hpp"
-#include "MCP9808.hpp"
-#include "TSL2591.hpp"
 
-using namespace mraa;
-
-	// Worker thread instances
+// Worker thread instances
 WorkerThread workerThread1("WorkerThread1");
 WorkerThread workerThread2("WorkerThread2");
 
-void sigalrm_handler(int sig)
-{
-	flag=1;
-}
-
-void check(int err)
-{
-	if (err<0) {
-		exit(-2);
-	}
-}
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
 int main(void)
-{
- 
-    // Create worker threads
-    workerThread1.CreateThread();
-    workerThread2.CreateThread();
+{	
+	// Create worker threads
+	workerThread1.CreateThread();
+	workerThread2.CreateThread();
 
-    // Create message to send to worker thread 1
-    UserData* userData1 = new UserData();
-    userData1->msg = "Hello world";
-    userData1->year = 2017;
+	// Create message to send to worker thread 1
+	UserData* userData1 = new UserData();
+	userData1->msg = "Hello world";
+	userData1->year = 2017;
 
-    // Post the message to worker thread 1
-    workerThread1.PostMsg(userData1);
+	// Post the message to worker thread 1
+	workerThread1.PostMsg(userData1);
 
-    // Create message to send to worker thread 2
-    UserData* userData2 = new UserData();
-    userData2->msg = "Goodbye world";
-    userData2->year = 2017;
+	// Create message to send to worker thread 2
+	UserData* userData2 = new UserData();
+	userData2->msg = "Goodbye world";
+	userData2->year = 2017;
 
-    // Post the message to worker thread 2
-    workerThread2.PostMsg(userData2);
+	// Post the message to worker thread 2
+	workerThread2.PostMsg(userData2);
 
-    // Give time for messages processing on worker threads
-    this_thread::sleep_for(1s);
+	// Give time for messages processing on worker threads
+	this_thread::sleep_for(1s);
 
-    workerThread1.ExitThread();
-    workerThread2.ExitThread();
+	workerThread1.ExitThread();
+	workerThread2.ExitThread();
 
-    return 0;
+	return 0;
 }
 
--- WorkerThread.cpp
+++ WorkerThread.cpp
@@ -1,163 +1,187 @@
+#include "WorkerThread.h"
+#include "Fault.h"
+#include <iostream>
+
+using namespace std;
+
+#define MSG_EXIT_THREAD			1
+#define MSG_POST_USER_DATA		2
+#define MSG_TIMER				3
+
+struct ThreadMsg
+{
+	ThreadMsg(int i, const void* m) { id = i; msg = m; }
+	int id;
+	const void* msg;
+};
+
+//----------------------------------------------------------------------------
+// WorkerThread
+//----------------------------------------------------------------------------
+WorkerThread::WorkerThread(const char* threadName) : m_thread(0), m_timerExit(false), THREAD_NAME(threadName)
+{
+}
+
+//----------------------------------------------------------------------------
+// ~WorkerThread
+//----------------------------------------------------------------------------
+WorkerThread::~WorkerThread()
+{
+	ExitThread();
+}
+
+//----------------------------------------------------------------------------
+// CreateThread
+//----------------------------------------------------------------------------
 bool WorkerThread::CreateThread()
 {
-    if (!m_thread)
-        m_thread = new thread(&WorkerThread::Process, this);
-    return true;
+	if (!m_thread)
+		m_thread = new thread(&WorkerThread::Process, this);
+	return true;
 }
 
-void WorkerThread::Process()
+//----------------------------------------------------------------------------
+// GetThreadId
+//----------------------------------------------------------------------------
+std::thread::id WorkerThread::GetThreadId()
 {
-    m_timerExit = false;
-    std::thread timerThread(&WorkerThread::TimerThread, this);
-
-    while (1)
-    {
-        ThreadMsg* msg = 0;
-        {
-            // Wait for a message to be added to the queue
-            std::unique_lock<std::mutex> lk(m_mutex);
-            while (m_queue.empty())
-                m_cv.wait(lk);
-
-            if (m_queue.empty())
-                continue;
-
-            msg = m_queue.front();
-            m_queue.pop();
-        }
-
-        switch (msg->id)
-        {
-            case MSG_POST_USER_DATA:
-            {
-                ASSERT_TRUE(msg->msg != NULL);
-
-                // Convert the ThreadMsg void* data back to a UserData*
-                const UserData* userData = static_cast<const UserData*>(msg->msg);
-
-                cout << userData->msg.c_str() << " " << userData->year << " on " << THREAD_NAME << endl;
-
-                // Delete dynamic data passed through message queue
-                delete userData;
-                delete msg;
-                break;
-            }
-
-            case MSG_TIMER:
-                cout << "Timer expired on " << THREAD_NAME << endl;
-                delete msg;
-                break;
-
-            case MSG_EXIT_THREAD:
-            {
-                m_timerExit = true;
-                timerThread.join();
-
-                delete msg;
-                std::unique_lock<std::mutex> lk(m_mutex);
-                while (!m_queue.empty())
-                {
-                    msg = m_queue.front();
-                    m_queue.pop();
-                    delete msg;
-                }
-
-                cout << "Exit thread on " << THREAD_NAME << endl;
-                return;
-            }
-
-            default:
-                ASSERT();
-        }
-    }
+	ASSERT_TRUE(m_thread != 0);
+	return m_thread->get_id();
 }
 
-void WorkerThread::PostMsg(const UserData* data)
+//----------------------------------------------------------------------------
+// GetCurrentThreadId
+//----------------------------------------------------------------------------
+std::thread::id WorkerThread::GetCurrentThreadId()
 {
-    ASSERT_TRUE(m_thread);
-
-    ThreadMsg* threadMsg = new ThreadMsg(MSG_POST_USER_DATA, data);
-
-    // Add user data msg to queue and notify worker thread
-    std::unique_lock<std::mutex> lk(m_mutex);
-    m_queue.push(threadMsg);
-    m_cv.notify_one();
+	return this_thread::get_id();
 }
 
+//----------------------------------------------------------------------------
+// ExitThread
+//----------------------------------------------------------------------------
 void WorkerThread::ExitThread()
 {
-    if (!m_thread)
-        return;
+	if (!m_thread)
+		return;
 
-    // Create a new ThreadMsg
-    ThreadMsg* threadMsg = new ThreadMsg(MSG_EXIT_THREAD, 0);
+	// Create a new ThreadMsg
+	ThreadMsg* threadMsg = new ThreadMsg(MSG_EXIT_THREAD, 0);
 
-    // Put exit thread message into the queue
-    {
-        lock_guard<mutex> lock(m_mutex);
-        m_queue.push(threadMsg);
-        m_cv.notify_one();
-    }
+	// Put exit thread message into the queue
+	{
+		lock_guard<mutex> lock(m_mutex);
+		m_queue.push(threadMsg);
+		m_cv.notify_one();
+	}
 
-    m_thread->join();
-    delete m_thread;
-    m_thread = 0;
+	m_thread->join();
+	delete m_thread;
+	m_thread = 0;
 }
 
-unsigned long WorkerThread::Process(void* parameter)
+//----------------------------------------------------------------------------
+// PostMsg
+//----------------------------------------------------------------------------
+void WorkerThread::PostMsg(const UserData* data)
 {
-    MSG msg;
-    BOOL bRet;
+	ASSERT_TRUE(m_thread);
 
-    // Start periodic timer
-    MMRESULT timerId = timeSetEvent(250, 10, &WorkerThread::TimerExpired, 
-                       reinterpret_cast<DWORD>(this), TIME_PERIODIC);
+	ThreadMsg* threadMsg = new ThreadMsg(MSG_POST_USER_DATA, data);
 
-    while ((bRet = GetMessage(&msg, NULL, WM_USER_BEGIN, WM_USER_END)) != 0)
-    {
-        switch (msg.message)
-        {
-            case WM_DISPATCH_DELEGATE:
-            {
-                ASSERT_TRUE(msg.wParam != NULL);
-
-                // Convert the ThreadMsg void* data back to a UserData*
-                const UserData* userData = static_cast<const UserData*>(msg.wParam);
-
-                cout << userData->msg.c_str() << " " << userData->year << " on " << THREAD_NAME << endl;
-
-                // Delete dynamic data passed through message queue
-                delete userData;
-                break;
-            }
-
-            case WM_USER_TIMER:
-                cout << "Timer expired on " << THREAD_NAME << endl;
-                break;
-
-            case WM_EXIT_THREAD:
-                timeKillEvent(timerId);
-                return 0;
-
-            default:
-                ASSERT();
-        }
-    }
-    return 0;
+	// Add user data msg to queue and notify worker thread
+	std::unique_lock<std::mutex> lk(m_mutex);
+	m_queue.push(threadMsg);
+	m_cv.notify_one();
 }
 
+//----------------------------------------------------------------------------
+// TimerThread
+//----------------------------------------------------------------------------
 void WorkerThread::TimerThread()
 {
-    while (!m_timerExit)
-    {
-        // Sleep for 250ms then put a MSG_TIMER message into queue
-        std::this_thread::sleep_for(250ms);
+	while (!m_timerExit)
+	{
+		// Sleep for 250ms then put a MSG_TIMER message into queue
+		std::this_thread::sleep_for(250ms);
 
-        ThreadMsg* threadMsg = new ThreadMsg(MSG_TIMER, 0);
+		ThreadMsg* threadMsg = new ThreadMsg(MSG_TIMER, 0);
 
-        // Add timer msg to queue and notify worker thread
-        std::unique_lock<std::mutex> lk(m_mutex);
-        m_queue.push(threadMsg);
-        m_cv.notify_one();
-    }
+		// Add timer msg to queue and notify worker thread
+		std::unique_lock<std::mutex> lk(m_mutex);
+		m_queue.push(threadMsg);
+		m_cv.notify_one();
+	}
 }
+
+//----------------------------------------------------------------------------
+// Process
+//----------------------------------------------------------------------------
+void WorkerThread::Process()
+{
+	m_timerExit = false;
+	std::thread timerThread(&WorkerThread::TimerThread, this);
+
+	while (1)
+	{
+		ThreadMsg* msg = 0;
+		{
+			// Wait for a message to be added to the queue
+			std::unique_lock<std::mutex> lk(m_mutex);
+			while (m_queue.empty())
+				m_cv.wait(lk);
+
+			if (m_queue.empty())
+				continue;
+
+			msg = m_queue.front();
+			m_queue.pop();
+		}
+
+		switch (msg->id)
+		{
+			case MSG_POST_USER_DATA:
+			{
+				ASSERT_TRUE(msg->msg != NULL);
+
+				// Convert the ThreadMsg void* data back to a UserData* 
+				const UserData* userData = static_cast<const UserData*>(msg->msg);
+
+				cout << userData->msg.c_str() << " " << userData->year << " on " << THREAD_NAME << endl;
+
+				// Delete dynamic data passed through message queue
+				delete userData;
+				delete msg;
+				break;
+			}
+
+			case MSG_TIMER:
+				cout << "Timer expired on " << THREAD_NAME << endl;
+				delete msg;
+				break;
+
+			case MSG_EXIT_THREAD:
+			{
+				m_timerExit = true;
+				timerThread.join();
+
+				delete msg;
+				std::unique_lock<std::mutex> lk(m_mutex);
+				while (!m_queue.empty())
+				{
+					msg = m_queue.front();
+					m_queue.pop();
+					delete msg;
+				}
+
+				cout << "Exit thread on " << THREAD_NAME << endl;
+				return;
+			}
+
+			default:
+				ASSERT();
+		}
+	}
+}
+
+
